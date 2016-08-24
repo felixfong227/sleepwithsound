b7ddc1eb4f9020603dc7afa87cd8611c4b66b79d
Felix Fong <moongod101@hotmail.com>
2016-08-24 11:35:18 +0800

    Add set time out function

--- www/js/script.update.js
+++ www/js/script.update.js
@@ -1,24 +1,24 @@
 var xhttp = new XMLHttpRequest();
-let localVersion = "2.1";
-  xhttp.onreadystatechange = function() {
+let localVersion = "2.2";
+xhttp.onreadystatechange = function() {
     if (xhttp.readyState == 4 && xhttp.status == 200) {
-      let back = xhttp.responseXML;
-      let remoteVersion = back.querySelector("widget").getAttribute("version");
+        let back = xhttp.responseXML;
+        let remoteVersion = back.querySelector("widget").getAttribute("version");
 
-      if(localVersion < remoteVersion){
-        //User need to update
+        if(localVersion < remoteVersion){
+            //User need to update
 
-        let div = document.createElement("div");
-          document.querySelector(".app").appendChild(div);
-          div.className = "updateNotif";
-          div.textContent = "New Update Available";
-          //When user click the link will send the users to update the app
-          div.addEventListener("click", () => {
-              window.open("https://build.phonegap.com/apps/2224767/download/android/","_self");
+            let div = document.createElement("div");
+            document.querySelector(".app").appendChild(div);
+            div.className = "updateNotif";
+            div.textContent = "New Update Available";
+            //When user click the link will send the users to update the app
+            div.addEventListener("click", () => {
+                window.open("https://build.phonegap.com/apps/2224767/download/android/","_self");
         });
-      }
+        }
 
     }
-  };
-  xhttp.open("GET", "https://rawgit.com/moongod101/sleepwithsound/master/config.xml", true);
-  xhttp.send();
+};
+xhttp.open("GET", "https://rawgit.com/moongod101/sleepwithsound/master/config.xml", true);
+xhttp.send();
