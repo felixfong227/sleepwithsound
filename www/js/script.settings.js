43c56c7538d154fcc9e2904190ad29bc2aa6d25c
Felix Fong <moongod101@hotmail.com>
2016-08-27 15:35:21 +0800

    2.3 Test

--- www/js/script.settings.js
+++ www/js/script.settings.js
@@ -9,8 +9,13 @@ document.addEventListener('DOMContentLoaded', function(){
 
     function check(){
         console.log(date);
-        if(date >= 22 || date == "00"){
+        if(date >= "22" || date >= "00" && date < "07"){
+            let el = document.createElement("div");
+            el.className = "sleepNotif";
+            document.querySelector(".app").appendChild(el);
             document.querySelector(".app .sleepNotif").textContent = "Time to sleep now";
+        }else{
+            document.querySelector(".app .sleepNotif").removeChild(el);
         }
     }
 
